d21f61e805704b0667677bc990c485085a83ce6b
pratamawijaya <set.mnemonix@gmail.com>
2017-09-02 14:11:37 +0700

    upgrade kotlin add experiment kotlin extension parcelable

--- app/src/main/java/com/pratamawijaya/opendotaarchcomponent/domain/Matches.kt
+++ app/src/main/java/com/pratamawijaya/opendotaarchcomponent/domain/Matches.kt
@@ -1,10 +1,16 @@
 package com.pratamawijaya.opendotaarchcomponent.domain
 
+import android.annotation.SuppressLint
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 /**
  * Created by pratama on 8/10/17.
  */
+@SuppressLint("ParcelCreator")
+@Parcelize
 data class Matches(val id: Long? = null,
                    val leagueName: String? = null,
                    val direName: String? = null,
                    val radiantName: String? = null,
-                   val duration: Int? = null)
+                   val duration: Int? = null) : Parcelable
